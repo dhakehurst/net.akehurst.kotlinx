4a393459aefb5664a80cd16b11b42e05bcab8b60
David Akehurst <david.akehurst@itemis.de>
2019-12-02 09:27:22 +0100

    kotlin 1.3.60

--- net.akehurst.kotlinx/kotlinx-collections/build.gradle.kts
+++ net.akehurst.kotlinx/kotlinx-collections/build.gradle.kts
@@ -1,29 +1,14 @@
 plugins {
-    id("net.akehurst.kotlin.kt2ts") version "1.0.0"
+    id("net.akehurst.kotlin.kt2ts") version "1.1.0"
 }
 
 dependencies {
         
 }
 
-val tsdDir ="${buildDir}/tmp/jsJar/ts"
-
-kotlin {
-    sourceSets {
-        val jsMain by getting {
-            resources.srcDir("${tsdDir}")
-        }
-    }
-}
-
 kt2ts {
-    outputDirectory.set(file(tsdDir))
     localJvmName.set("jvm8")
-    modulesConfigurationName.set("jvm8RuntimeClasspath")
     classPatterns.set(listOf(
             "net.akehurst.kotlinx.collections.*"
     ))
 }
-
-tasks.getByName("generateTypescriptDefinitionFile").dependsOn("jvm8MainClasses")
-tasks.getByName("jsJar").dependsOn("generateTypescriptDefinitionFile")
--- net.akehurst.kotlinx/kotlinx-reflect/build.gradle.kts
+++ net.akehurst.kotlinx/kotlinx-reflect/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("net.akehurst.kotlin.kt2ts") version "1.0.0"
+    id("net.akehurst.kotlin.kt2ts") version "1.1.0"
 }
 
 dependencies {
@@ -8,25 +8,10 @@ dependencies {
     jvm8MainImplementation(kotlin("reflect"))
 }
 
-
-val tsdDir = "${buildDir}/tmp/jsJar/ts"
-
-kotlin {
-    sourceSets {
-        val jsMain by getting {
-            resources.srcDir("${tsdDir}")
-        }
-    }
-}
-
 kt2ts {
-    outputDirectory.set(file(tsdDir))
     localJvmName.set("jvm8")
-    modulesConfigurationName.set("jvm8RuntimeClasspath")
     classPatterns.set(listOf(
             "net.akehurst.kotlinx.reflect.*"
     ))
 }
 
-tasks.getByName("generateTypescriptDefinitionFile").dependsOn("jvm8MainClasses")
-tasks.getByName("jsJar").dependsOn("generateTypescriptDefinitionFile")
