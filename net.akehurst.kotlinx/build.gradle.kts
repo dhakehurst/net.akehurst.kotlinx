968bb0bece7daa1f766447e678256ae00ddcaf39
David Akehurst <david.akehurst@itemis.de>
2022-06-30 20:26:54 +0200

    bump version kotlin

--- net.akehurst.kotlinx/build.gradle.kts
+++ net.akehurst.kotlinx/build.gradle.kts
@@ -19,10 +19,10 @@ import com.github.gmazzo.gradle.plugins.BuildConfigExtension
 import java.io.File
 
 plugins {
-    kotlin("multiplatform") version ("1.6.0") apply false
-    id("net.akehurst.kotlin.gradle.plugin.exportPublic") version("1.6.0") apply false
-    id("org.jetbrains.dokka") version ("1.6.0") apply false
-    id("com.github.gmazzo.buildconfig") version("3.0.0") apply false
+    kotlin("multiplatform") version ("1.7.0") apply false
+    id("net.akehurst.kotlin.gradle.plugin.exportPublic") version("1.7.0") apply false
+    id("org.jetbrains.dokka") version ("1.7.0") apply false
+    id("com.github.gmazzo.buildconfig") version("3.1.0") apply false
     id("nu.studer.credentials") version ("3.0")
 }
 
